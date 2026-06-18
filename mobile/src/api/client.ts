5c8e15c0d187cca0be9d58bc054a4a07b5a41499
sogoyou8 <yoann.sogoyou.pro@gmail.com>
2026-06-18 11:29:04 +0200

    fix(mobile): update API base URL to current Wi-Fi IP address

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 // Change this to your local machine IP when testing on a physical device
 // e.g. 'http://192.168.1.10:3000/api'
-export const API_BASE_URL = 'http://192.168.1.92:3000/api'
+export const API_BASE_URL = 'http://10.92.6.69:3000/api'
 
 const client = axios.create({
   baseURL: API_BASE_URL,
